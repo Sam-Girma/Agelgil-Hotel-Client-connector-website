60adb70057d2a0fcbfffbeff1333776631c55b7d
Samuel Girma Megra <samigirma53@gmail.com>
2022-01-26 20:57:40 +0300

    admin and client side adjusted.

--- src/main/java/com/agelgil/agelgil/client/client/data/models/Client.java
+++ src/main/java/com/agelgil/agelgil/client/client/data/models/Client.java
@@ -1,11 +1,11 @@
-package com.agelgil.agelgil.admin.data.models;
+package com.agelgil.agelgil.client.data.models;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
@@ -16,32 +16,32 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 
-@NoArgsConstructor
 @Data
+@NoArgsConstructor
 @Entity
 @Table(
-	name = "admin_admin",
+	name = "client_client",
 	uniqueConstraints = @UniqueConstraint(columnNames = {"user_username"})
 	)
-public class Admin extends UserType{
-	
+public class Client extends UserType{
+
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
-	@JoinColumn(nullable = false, unique = true)
 	@ManyToOne
 	private User user;
-	
-	private String firstName;
-	private String lastName;
+
+	private String fullName;
+
+	@OneToOne(mappedBy = "client")
+	private Cart cart;
 
 
-	public Admin(User user, String firstName, String lastName){
+	public Client(User user, String fullName){
 		this.user = user;
-		this.firstName = firstName;
-		this.lastName = lastName;
+		this.fullName = fullName;
 	}
 
-	
 }
